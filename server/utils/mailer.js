57c167e8a48878799a8b390741ef3e891d77fb38
Twisha Patel <twipatel2006@gmail.com>
2026-07-23 17:27:00 +0530

    Update

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,11 +1,14 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4, 
 });
 
 export default transporter;
